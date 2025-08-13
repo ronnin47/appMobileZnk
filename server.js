8a591eae38dca21859dbd5ac708200d9cfc11ad6
jorge tournier <tempesttempest66l@gmail.com>
2025-08-12 21:31:13 -0300

    unificacion 1

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
 
-//SOCKETIO
+//SOCKETIO esta es la prueba para construir
 const server = http.createServer(app);
 // Inicializa el servidor de Socket.IO
 const io = new Server(server, {
